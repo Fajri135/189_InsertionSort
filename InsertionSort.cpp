3f82bc1f2b422ec34ecc0a4c3d1ab3974cee6684
Fajri135 <corex135@gmail.com>
2024-03-14 12:27:03 +0700

    Membuat Procedure Display

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -57,6 +57,19 @@ void insertionsort() {                                      // Procedure Inserti
     }
 }
 
+void display() {                                             // Procedure Display
+    cout << endl;                                           // Output baris kosong
+    cout << "\n===============================" << endl;    // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;    // Output ke layar
+    cout << "=================================" << endl;    // Output ke layar
+
+    for (int j = 0; j < n; j++) {                           // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl;                             // Output ke layar
+    }
+    cout << endl;                                           // Output baris kosong
+
+}
+
     int main(){
         
 }
